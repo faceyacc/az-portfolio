08ecf62b8fc2f00b875d5bd43fd75147fbab83d8
Ty <justfacey@gmail.com>
2023-03-07 17:44:09 -0800

    added correct styling to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -80,7 +80,7 @@ const ClosedComissions = () => {
           {/* END FIELDS */}
       </div>
 
-      <div class="mt-40 text-center">
+      <div class="text-center">
         <h1 class=" text-2xl text-gray-900 md:text-4xl lg:text-4xl dark:text-grey mb-10">
           Commissions are CLOSED
         </h1>
@@ -88,7 +88,7 @@ const ClosedComissions = () => {
           Hi! We are excited to announce that we are accepting commission orders
           on:
         </p>
-        <p class="dark:text-grey text-gray-900 mb-4 ml-10 text-2xl break-normal">
+        <p class="dark:text-grey text-gray-900 mb-4  text-2xl break-normal">
           March 17th <br/>
           Friday <br/>
           12 PM EST
