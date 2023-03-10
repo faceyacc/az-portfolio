018dfc890ab8404222c036638fbd30882066deca
Ty <justfacey@gmail.com>
2023-03-09 21:24:44 -0800

    testing

--- pages/contact.js
+++ pages/contact.js
@@ -80,7 +80,7 @@ const ClosedComissions = () => {
           {/* END FIELDS */}
       </div>
 
-      <div class="text-center">
+      <div class=" text-center font-family: Poppins">
         <h1 class=" text-2xl text-gray-900 md:text-4xl lg:text-4xl dark:text-grey mb-10">
           Commissions are CLOSED
         </h1>
@@ -88,7 +88,7 @@ const ClosedComissions = () => {
           Hi! We are excited to announce that we are accepting commission orders
           on:
         </p>
-        <p class="dark:text-grey text-gray-900 mb-4  text-2xl break-normal">
+        <p class="dark:text-grey text-gray-900 mb-4  text-xl break-normal">
           March 17th <br/>
           Friday <br/>
           12 PM EST
